d1b55934b8ae43886d2f172b8725498685ac0063
Philin01 <philin02@mail.ru>
2025-07-16 20:49:52 +0300

    Build 0.9.2

---
+++ Source/SnakeGame/Public/GameRecord.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameRecord.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAME_API UGameRecord : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
